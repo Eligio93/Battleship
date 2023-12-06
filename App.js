0551a08b557ff34f27b9fd59063575c88c74c587
Eligio93 <eligio.cristantielli@gmail.com>
2023-12-06 17:18:06 +1100

    creates gameboard and receive attack function

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 class Ship{
-    constructor(length){
+    constructor(length,position){
         this.length=length,
-        this.x=0;
-        this.y=0;
+        // this.x=0;
+        // this.y=0;
         this.hits=0;
+        this.coordinates=["A1","A2","A3","A4"];
+        // this.position=position;
         this.sunk=false;
     }
     hit(){
@@ -18,7 +20,32 @@ class Ship{
         }
     }
 }
+class Gameboard{
+    constructor(player){
+        this.player=player
+        this.shipsNumber=4;
+        this.ships=[new Ship(4)];
+        this.missingCoordinates=[]
+    }
+    receiveAttack(coordinates){
+        let found=false
+        this.ships.forEach(object=>{
+        if(object.coordinates.includes(coordinates)){           
+            found=true;
+            object.hit();
+            if(object.isSunk()){
+                this.shipsNumber-=1
+            }
+        }
+      })
+      if(found==false){
+        this.missingCoordinates.push(coordinates);
+      }
+      return found
+    }
+}
 
-let boat= new Ship(4);
+let gameboard1=new Gameboard();
+// gameboard1.receiveAttack("A1");
 
-module.exports=boat;
+module.exports=gameboard1;
