abae91969b70bf8856a54146cae537eae3f3e197
Eligio93 <eligio.cristantielli@gmail.com>
2023-12-06 17:31:14 +1100

    test missing shot and if all ships are sunk

--- App.test.js
+++ App.test.js
@@ -11,3 +11,15 @@ test('Sunk boat', () =>{
     gameboard1.receiveAttack("A4");
     expect(gameboard1.ships[0].sunk).toBeTruthy()
 })
+test('missing shot', ()=>{
+    gameboard1.receiveAttack("B1")
+    expect(gameboard1.missingCoordinates[0]).toBe("B1")
+})
+test('all ships are sunk',()=>{
+    gameboard1.receiveAttack("A1");
+    gameboard1.receiveAttack("A2");
+    gameboard1.receiveAttack("A3");
+    gameboard1.receiveAttack("A4");
+    expect( gameboard1.shipsNumber).toBe(0);
+
+})
