ed441bdecc45c5aad9de3f65ab301de219ab428a
Eligio93 <eligio.cristantielli@gmail.com>
2023-12-06 17:19:48 +1100

    test fro sung ship

--- App.test.js
+++ App.test.js
@@ -9,4 +9,5 @@ test('Sunk boat', () =>{
     gameboard1.receiveAttack("A2");
     gameboard1.receiveAttack("A3");
     gameboard1.receiveAttack("A4");
+    expect(gameboard1.ships[0].sunk).toBeTruthy()
 })
