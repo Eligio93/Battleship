05608c6bfbdfcd5c47b5eb0dc5df9169b9dcfe86
Eligio93 <eligio.cristantielli@gmail.com>
2023-12-06 17:16:29 +1100

    test ship being hitted

---
+++ App.test.js
@@ -0,0 +1,12 @@
+const gameboard1 = require("./App");
+
+test('hitted boat', () => {
+    gameboard1.receiveAttack("A1");
+    expect(gameboard1.ships[0].hits).toBe(1);
+});
+test('Sunk boat', () =>{
+    gameboard1.receiveAttack("A1");
+    gameboard1.receiveAttack("A2");
+    gameboard1.receiveAttack("A3");
+    gameboard1.receiveAttack("A4");
+})
