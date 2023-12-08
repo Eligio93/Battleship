1de8b932ad0e21ba1da074999cc22bac50029843
Eligio93 <eligio.cristantielli@gmail.com>
2023-12-08 12:55:33 +1100

    Improves tests logic

--- App.test.js
+++ App.test.js
@@ -1,25 +1,30 @@
-const gameboard1 = require("./App");
+let {Gameboard,Ship} = require("./App");
+
 
 test('hitted boat', () => {
-    gameboard1.receiveAttack("A1");
+    let gameboard1= new Gameboard("Player1", new Ship(4))
+    gameboard1.receiveAttack("00");
     expect(gameboard1.ships[0].hits).toBe(1);
 });
 test('Sunk boat', () =>{
-    gameboard1.receiveAttack("A1");
-    gameboard1.receiveAttack("A2");
-    gameboard1.receiveAttack("A3");
-    gameboard1.receiveAttack("A4");
+    let gameboard1= new Gameboard("Player1", new Ship(4))
+    gameboard1.receiveAttack("00");
+    gameboard1.receiveAttack("01");
+    gameboard1.receiveAttack("02");
+    gameboard1.receiveAttack("03");
     expect(gameboard1.ships[0].sunk).toBeTruthy()
 })
 test('missing shot', ()=>{
-    gameboard1.receiveAttack("B1")
-    expect(gameboard1.missingCoordinates[0]).toBe("B1")
+    let gameboard1= new Gameboard("Player1", new Ship(4))
+    gameboard1.receiveAttack("25")
+    expect(gameboard1.missingCoordinates[0]).toBe("25")
 })
 test('all ships are sunk',()=>{
-    gameboard1.receiveAttack("A1");
-    gameboard1.receiveAttack("A2");
-    gameboard1.receiveAttack("A3");
-    gameboard1.receiveAttack("A4");
+    let gameboard1= new Gameboard("Eligio", new Ship(4))
+    gameboard1.receiveAttack("00");
+    gameboard1.receiveAttack("01");
+    gameboard1.receiveAttack("02");
+    gameboard1.receiveAttack("03");
     expect( gameboard1.shipsNumber).toBe(0);
 
 })
