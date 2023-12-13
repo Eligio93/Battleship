c6684e334517fe86a7b6ed443c2bff2c5f7c414a
Eligio93 <eligio.cristantielli@gmail.com>
2023-12-14 01:20:38 +1100

    test change player s turn

--- src/App.test.js
+++ src/App.test.js
@@ -52,3 +52,29 @@ test("all ships are sunk", ()=>{
     user.attackOpponent("84",computerGameboard)
     expect(computerGameboard.shipsNumber).toBe(0)
 })
+test("change player's turn", ()=>{
+    let user= new Player("user", true);
+    let computer= new Player("computer" ,false);
+    let userShips=[new Ship(4,["00","01","02","03"]),new Ship(4,["21","22","23","24"])]
+    let computerShips=[new Ship(4,["30","31","32","33"]),new Ship(4,["81","82","83","84"])]
+    let userGameboard=new Gameboard(user,userShips);
+    let computerGameboard=new Gameboard(computer,computerShips);
+    expect(user.active).toBeTruthy();
+    expect(computer.active).toBeFalsy();
+    user.attackOpponent("30",computerGameboard);
+    expect(user.active).toBeTruthy();
+    expect(computer.active).toBeFalsy();
+    user.attackOpponent("29",computerGameboard);
+    expect(user.active).toBeFalsy();
+    expect(computer.active).toBeTruthy();
+    computer.attackOpponent("00",userGameboard);
+    expect(computer.active).toBeTruthy();
+    expect(user.active).toBeFalsy();
+    computer.attackOpponent("21",userGameboard);
+    expect(computer.active).toBeTruthy();
+    expect(user.active).toBeFalsy();
+    computer.attackOpponent("90",userGameboard);
+    expect(computer.active).toBeFalsy();
+    expect(user.active).toBeTruthy();
+    
+})
