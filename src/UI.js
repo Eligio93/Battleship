2dbea7c0c9920625346096e1a500065ba83ed5fd
Eligio93 <eligio.cristantielli@gmail.com>
2023-12-14 16:20:38 +1100

    add functions to display and disable gameboard

---
+++ src/UI.js
@@ -0,0 +1,34 @@
+function displayGameboard(gameboard){
+    let shipsCoordinates=(gameboard.ships.map(obj=>obj.coordinates)).flat();
+    console.log(shipsCoordinates);
+    for(let i=0;i<10;i++){
+        for(let j=0;j<10;j++){
+            let cell=document.createElement("div");
+            cell.className="cell";
+            cell.textContent=i+''+j;
+            cell.setAttribute("data-coordinates",i+''+j);
+            if(shipsCoordinates.includes(cell.getAttribute("data-coordinates"))/*&& gameboard.player.name!=="computer"*/){
+                cell.style.backgroundColor="green"
+            }
+            if(gameboard.player.name=="computer"){
+                document.getElementById("computer-side").appendChild(cell);
+            }else{
+                document.getElementById("player-side").appendChild(cell);
+            }
+        }
+    }
+
+
+}
+function disableGameboard(gameboard){
+   if(gameboard.player.name=="computer"){
+    document.getElementById("computer-side").style.pointerEvents="none";
+    document.getElementById("player-side").style.pointerEvents="auto";
+   }else{
+    document.getElementById("computer-side").style.pointerEvents="auto";
+    document.getElementById("player-side").style.pointerEvents="none";
+   }
+}
+
+
+export {displayGameboard,disableGameboard}
