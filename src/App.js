35f3265167eacea85310877de1c178e1021d2212
Eligio93 <eligio.cristantielli@gmail.com>
2023-12-21 22:58:51 +1100

    improves receiveAttack and attackOpponent functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //creates Ship class
-import { disableGameboard, styleCell,displayWinner } from "./UI";
+import { disableGameboard, styleCell, displayWinner } from "./UI";
 class Ship {
     constructor(length, coordinates) {
         this.length = length;
@@ -30,54 +30,68 @@ class Gameboard {
     }
 
     receiveAttack(coordinates) {
+        //variable to check if the ship is been found or not
         let found = false;
         if (this.totalShots.includes(coordinates)) {
-            return null; //"Shoot again"
+            //if the shot is been already done the function returns null
+            return null;
         };
-
+        //check in each ship if the coordinates are matching with the ship
         this.ships.forEach(ship => {
+            //if found the function returns true
             if (ship.coordinates.includes(coordinates)) {
-                this.totalShots.push(coordinates);
                 found = true;
+                this.totalShots.push(coordinates);
                 ship.hit();
-                styleCell(this,coordinates,found)
                 if (ship.isSunk()) {
                     this.shipsNumber -= 1;
-                    if(this.shipsNumber==0){
-                        displayWinner(this);
-                    }
                 }
             }
         })
+        //if the shot is valid but doesn t find ship returns false
         if (found == false) {
             this.totalShots.push(coordinates);
-            styleCell(this,coordinates,found)
         }
         return found
-
     }
 }
 
 class Player {
     constructor(name, active) {
         this.name = name,
-            this.active = active
+        this.active = active
     }
+
     attackOpponent(coordinates, opponentGameboard) {
+        //if the shot is valid change turn and return false
         if (opponentGameboard.receiveAttack(coordinates) == false) {
             opponentGameboard.player.active = true;
-            disableGameboard(opponentGameboard);//disable gameboard of the active player
             this.active = false;
+            return false
+        }else if(opponentGameboard.receiveAttack(coordinates) == true){
+            //the shot hited a ship and turn is not changing
+            return true
+        }else{
+            //not valid shot so no changing turn
+            return null
         }
-
     }
 }
-function generateComputerCoordinates(){
-    let coordinates=Math.floor(Math.random()*100);
-    if(coordinates<10){
-        coordinates='0'+coordinates
-    }
-    return coordinates;
+
+
+function userRound(user, userGameboard, computerGameboard) {
+    disableGameboard(userGameboard);
+    document.getElementById("computer-side").addEventListener("click", function (event) {
+        let coordinates = event.target.getAttribute("data-coordinates");
+        user.attackOpponent(coordinates, computerGameboard);
+    })
+}
+
+function computerRound(computer, computerGameboard, userGameboard) {
+    disableGameboard(computerGameboard);
+    let coordinates = generateComputerCoordinates();
+    console.log(coordinates)
+    computer.attackOpponent(coordinates, userGameboard)
 }
 
 
@@ -90,7 +104,7 @@ let playRound = function (user, userGameboard, computer, computerGameboard) {
                 console.log(coordinates)
                 user.attackOpponent(coordinates, computerGameboard);
             } else {
-                coordinates=generateComputerCoordinates();
+                // coordinates=generateComputerCoordinates();
                 console.log(coordinates)
                 computer.attackOpponent(coordinates, userGameboard)
             }
@@ -99,5 +113,34 @@ let playRound = function (user, userGameboard, computer, computerGameboard) {
 }
 
 
-export { Ship, Gameboard, Player, playRound }
+
+
+// function generateComputerCoordinates(){
+//     let coordinates=Math.floor(Math.random()*100);
+//     if(coordinates<10){
+//         coordinates='0'+coordinates
+//     }
+//     return coordinates;
+// }
+
+
+// let playRound = function (user, userGameboard, computer, computerGameboard) {
+//     disableGameboard(userGameboard);
+//     document.querySelectorAll(".game-section").forEach(element => {
+//         element.addEventListener("click", function (event) {
+//             let coordinates = event.target.getAttribute("data-coordinates");
+//             if (user.active == true) {
+//                 console.log(coordinates)
+//                 user.attackOpponent(coordinates, computerGameboard);
+//             } else {
+//                 // coordinates=generateComputerCoordinates();
+//                 console.log(coordinates)
+//                 computer.attackOpponent(coordinates, userGameboard)
+//             }
+//         })
+//     })
+// }
+
+
+export { Ship, Gameboard, Player, /*playRound,*/ userRound }
 
