33eaee0877facba2d20c8ede60cc6e42acd61fa8
Eligio93 <eligio.cristantielli@gmail.com>
2023-12-27 13:26:21 +1100

    adds function able to style cell after attack

--- src/UI.js
+++ src/UI.js
@@ -1,18 +1,18 @@
-function displayGameboard(gameboard){
-    let shipsCoordinates=(gameboard.ships.map(obj=>obj.coordinates)).flat();
+function displayGameboard(gameboard) {
+    let shipsCoordinates = (gameboard.ships.map(obj => obj.coordinates)).flat();
     console.log(shipsCoordinates);
-    for(let i=0;i<10;i++){
-        for(let j=0;j<10;j++){
-            let cell=document.createElement("div");
-            cell.className="cell";
-            cell.textContent=i+''+j;
-            cell.setAttribute("data-coordinates",i+''+j);
-            if(shipsCoordinates.includes(cell.getAttribute("data-coordinates"))/*&& gameboard.player.name!=="computer"*/){
-                cell.style.backgroundColor="green"
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            let cell = document.createElement("div");
+            cell.className = "cell";
+            cell.textContent = i + '' + j;
+            cell.setAttribute("data-coordinates", i + '' + j);
+            if (shipsCoordinates.includes(cell.getAttribute("data-coordinates"))/*&& gameboard.player.name!=="computer"*/) {
+                cell.style.backgroundColor = "green"
             }
-            if(gameboard.player.name=="computer"){
+            if (gameboard.player.name == "computer") {
                 document.getElementById("computer-side").appendChild(cell);
-            }else{
+            } else {
                 document.getElementById("player-side").appendChild(cell);
             }
         }
@@ -20,38 +20,39 @@ function displayGameboard(gameboard){
 
 
 }
-function disableGameboard(gameboard){
-   if(gameboard.player.name=="computer"){
-    document.getElementById("computer-side").style.pointerEvents="none";
-    document.getElementById("player-side").style.pointerEvents="auto";
-   }else{
-    document.getElementById("computer-side").style.pointerEvents="auto";
-    document.getElementById("player-side").style.pointerEvents="none";
-   }
-}
-function styleCell(gameboard,coordinates,found){
-    let cell;
-    if(gameboard.player.name=="computer"){
-        cell=document.getElementById("computer-side").querySelector('[data-coordinates="'+coordinates+'"]');
-    }else{
-        cell=document.getElementById("player-side").querySelector('[data-coordinates="'+coordinates+'"]');
-    }
-    if(found==true){           
-        cell.style.backgroundColor="pink";
-    }else if(found==false){
-        cell.style.backgroundColor="blue";
+function disableGameboard(gameboard) {
+    if (gameboard.player.name == "computer") {
+        document.getElementById("computer-side").style.pointerEvents = "none";
+        document.getElementById("player-side").style.pointerEvents = "auto";
+    } else {
+        document.getElementById("computer-side").style.pointerEvents = "auto";
+        document.getElementById("player-side").style.pointerEvents = "none";
     }
 }
 
-function displayWinner(loserGameboard){
-    document.getElementById("player-side").style.pointerEvents="none";
-    document.getElementById("computer-side").style.pointerEvents="none";
-    if(loserGameboard.player.name=="computer"){
+function styleCell(player, attackResult, coordinates) {
+    let cell;
+    if (player.name == "user") {
+        cell = document.getElementById("computer-side").querySelector('[data-coordinates="' + coordinates + '"]');
+    } else {
+        cell = document.getElementById("player-side").querySelector('[data-coordinates="' + coordinates + '"]');
+    }
+    if (attackResult == true) {
+        cell.style.backgroundColor = "pink";
+    } else if (attackResult == false) {
+        cell.style.backgroundColor = "blue";
+    }
+}
+
+function displayWinner(loserGameboard) {
+    document.getElementById("player-side").style.pointerEvents = "none";
+    document.getElementById("computer-side").style.pointerEvents = "none";
+    if (loserGameboard.player.name == "computer") {
         console.log("you win")
-    }else{
+    } else {
         console.log("computer wins")
     }
 }
 
 
-export {displayGameboard,disableGameboard,styleCell,displayWinner}
+export { displayGameboard, disableGameboard, styleCell, displayWinner }
